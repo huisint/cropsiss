558b85afad269d7ddb591e999aff9d722028844d
Huisin <huisintheta@gmail.com>
2022-07-16 15:37:00 +0000

    Release v0.3.1

--- cropsiss/__init__.py
+++ cropsiss/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2022 Shuhei Nitta. All rights reserved.
 """Cross Platform Simultaneously Selling System"""
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 from cropsiss import platforms
 
